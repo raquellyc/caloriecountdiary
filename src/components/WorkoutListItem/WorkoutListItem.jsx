a4f7991b70f4c35114f92ef1ad52e58765c2eb4a
raquellyc <raquels.castillo.78@gmail.com>
2022-03-10 10:06:38 -0700

    Deploy

--- src/components/WorkoutListItem/WorkoutListItem.jsx
+++ src/components/WorkoutListItem/WorkoutListItem.jsx
@@ -2,7 +2,6 @@
 export default function WorkoutListItem({ item }){
     return(
         <ul className="WorkoutListItem">
-            <h1>workout item</h1>
             <li>{item.name} </li>
             <li className="time"> Start-Time: {item.time} </li>
             <li className="duration"> duration: {item.duration} </li>
